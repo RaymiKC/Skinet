66e93373592163bc92cfe1339b1cac962d641d4e
RAMIRAK_RD <ramirak.rd@gmail.com>
2022-06-22 14:15:20 -0400

    end of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,16 +6,17 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController: ControllerBase {
 
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context) {
-            _context = context;
+        public ProductsController(IProductRepository repo) {
+            _repo = repo;
 
         }
 
@@ -23,7 +24,7 @@ namespace API.Controllers {
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts() {
 
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductAsync();
 
             return Ok(products);
         }
@@ -31,7 +32,28 @@ namespace API.Controllers {
         [HttpGet("{id}")]
 
         public async Task<ActionResult<Product>> GetProduct(int id) {
-            return await _context.Products.FindAsync(id);
+            
+            var asyncList = await _repo.GetProductByIdAsync(id);
+
+            return Ok(asyncList);
+        }
+
+        [HttpGet("types")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes() {
+            
+            var asyncList = await _repo.GetProductTypesAsync();
+
+            return Ok(asyncList);
+        }
+
+        [HttpGet("brands")]
+
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands() {
+            
+            var asyncList = await _repo.GetProductBrandsAsync();
+
+            return Ok(asyncList);
         }
     }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Infrastructure.Data {
     public class StoreContext: DbContext {
         public StoreContext(DbContextOptions <StoreContext> options): base(options) {}
 
-        public DbSet <Product> Products {get;set;}
+        public DbSet <Product> Products { get; set; }
+        public DbSet <ProductBrand> ProductBrands { get; set; }
+        public DbSet <ProductType> ProductTypes { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
